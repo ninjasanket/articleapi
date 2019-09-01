ca639281ef9eeb9d5ed5c8ab2cd0b2416d08dc2d
root <you@example.com>
2019-09-02 01:55:37 +0530

    auth token files commited

---
+++ application/controllers/api/Login.php
@@ -0,0 +1,43 @@
+<?php
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Login extends REST_Controller
+{
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(['jwt', 'authorization']);
+    }
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+     */
+
+    public function index_post()
+    {
+        $dummy_user = [
+            'username' => 'ninjasanket',
+            'password' => 'gravityworrier',
+        ];
+        $username = $this->post('username');
+        $password = $this->post('password');
+        if (trim($username) === $dummy_user['username'] && trim($password) === $dummy_user['password']) {
+            $token = AUTHORIZATION::generateToken(['username' => $dummy_user['username']]);
+            $status = parent::HTTP_OK;
+            $response = ['status' => $status, 'token' => $token];
+            $this->response($response, $status);
+        } else {
+            $this->response(['msg' => 'Invalid username or password!'], parent::HTTP_NOT_FOUND);
+        }
+    }
+}
+
